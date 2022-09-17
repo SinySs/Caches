7b09c523b5518dab4c72e09d8c22d0c9c42bd239
SinySs <dimakrytoi8@gmail.com>
2022-09-17 21:19:47 +0300

    regroup files and add LFU

---
+++ LFU/LFU.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <iterator>
+#include <unordered_map>
+#include <map>
+
+namespace cache
+{
+const size_t START_TIME = 1;
+
+template <typename T, typename KeyT = int> class lfu_cache_t
+{
+    struct data
+    {
+        T data_;
+        KeyT key_;
+    };
+
+
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+    using lfu_iterator = typename std::multimap<size_t, data>::iterator;
+
+
+    std::multimap<size_t, data> freq_map_;
+    std::unordered_map<KeyT, lfu_iterator> hash_;
+
+    void erase()
+    {
+        KeyT rem_key = freq_map_.begin()->second.key_;
+        freq_map_.erase(hash_[rem_key]);
+        hash_.erase(rem_key);
+        size_--;
+    }
+
+    template <typename F> void insert(KeyT key, F slow_get_page_int)
+    {
+        data new_elem;
+        new_elem.data_ = slow_get_page_int(key);
+        new_elem.key_ = key;
+        if(full()) {
+            erase();
+        }
+
+        hash_[key] = freq_map_.emplace_hint(freq_map_.begin(), START_TIME, new_elem);
+        size_++;
+    }
+
+    void update(KeyT key, lfu_iterator hit)
+    {
+        auto update_elem = std::make_pair(hit->first + 1, hit->second);
+        freq_map_.erase(hit);
+        hash_[key] = freq_map_.emplace_hint(freq_map_.end(), update_elem);
+    }
+
+public:
+    lfu_cache_t(size_t cache_size) : capacity_(cache_size) {}
+
+    bool full() { return size_ == capacity_; }
+
+    template <typename F> bool loookup_update(KeyT key, F slow_get_page_int)
+    {
+        auto hit = hash_.find(key);
+
+        if(hit == hash_.end()) {
+            insert(key, slow_get_page_int);
+            return false;
+        }
+
+        update(key, hit->second);
+
+        return true;
+    }
+
+};
+} // namespace cache
---
+++ LFU/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <assert.h>
+
+#include "LFU.hpp"
+
+int slow_get_page_int(int key) { return key; }
+
+int main()
+{
+    size_t m;
+    int n;
+    int hits = 0;
+
+    std::cin >> n >> m;
+    assert(std::cin.good());
+
+    cache::lfu_cache_t<int> cache{m};
+
+    for(int i = 0; i < n; i++) {
+        int q;
+        std::cin >> q;
+        assert(std::cin.good());
+
+        if(cache.loookup_update(q, slow_get_page_int))
+            hits++;
+    }
+
+    std::cout << hits << std::endl;
+
+    return 0;
+}
